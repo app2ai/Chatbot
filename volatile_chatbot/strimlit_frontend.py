0c2e80c920bb66795f64e3d0e4463ba69f807668
Vishal Rana <job.vishalrana@gmail.com>
2026-08-05 21:24:12 +0530

    add sqlite in req file

--- volatile_chatbot/strimlit_frontend.py
+++ volatile_chatbot/strimlit_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from volatile_chatbot.langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage, AIMessage
 import uuid
 
