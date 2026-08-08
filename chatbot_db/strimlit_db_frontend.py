143567a2b9e5b39aa3a6e42bb7d0a80e3e3802b9
Vishal Rana <job.vishalrana@gmail.com>
2026-08-08 15:20:16 +0530

    Added langsmith to trace chatbot

--- chatbot_db/strimlit_db_frontend.py
+++ chatbot_db/strimlit_db_frontend.py
@@ -80,7 +80,11 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG = {
+        'configurable': {'thread_id': st.session_state['thread_id']},
+        'metadata': {'thread_id': st.session_state['thread_id']},
+        'run_name': "Chat turn"
+        }
 
      # first add the message to message_history
     with st.chat_message("assistant"):
